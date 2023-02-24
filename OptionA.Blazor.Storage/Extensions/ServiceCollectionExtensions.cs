6576ef8724b6ed274064fb675e90b9b2b450db39
Erik van der Boom <evdboom@hotmail.com>
2023-02-25 00:00:17 +0100

    Split database and fileservice extensions

--- OptionA.Blazor.Storage/Extensions/ServiceCollectionExtensions.cs
+++ OptionA.Blazor.Storage/Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using OptionA.Blazor.Storage.Interfaces;
 using OptionA.Blazor.Storage.Services;
 using OptionA.Blazor.Storage.Utilities;
@@ -15,19 +16,31 @@ namespace OptionA.Blazor.Storage.Extensions
         /// <summary>
         /// Adds the following services to the container:
         /// <para><see cref="ServiceLifetime.Scoped"/> <see cref="IStorageService"/> for storing and retrieving from local and or session storage</para>
-        /// <para><see cref="ServiceLifetime.Scoped"/> <see cref="IDatabaseService"/> for storing and retrieving from indexed db</para>
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddStorageService(this IServiceCollection services)
+        {
+            services
+                .AddScoped<IStorageService, StorageService>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the following services to the container:        
+        /// <para><see cref="ServiceLifetime.Scoped"/> <see cref="IDatabaseService"/> for storing and retrieving from indexed db for the database of the given config <see cref="StorageOptions"/></para>
         /// </summary>
         /// <param name="services"></param>
         /// <param name="configuration"></param>
         /// <param name="optionsKey"></param>
         /// <returns></returns>
-        public static IServiceCollection AddStorageServices(this IServiceCollection services, IConfiguration configuration, string optionsKey)
+        public static IServiceCollection AddDatabaseService(this IServiceCollection services, IConfiguration configuration, string optionsKey)
         {
-            services
-                .AddScoped<IStorageService, StorageService>()
-                .AddScoped<IDatabaseService, DatabaseService>()
-                .AddSingleton<MigrationBuilder>()
-                .Configure<StorageOptions>(options => configuration.Bind(optionsKey, options));
+            services                
+                .AddScoped<IDatabaseService, DatabaseService>()                
+                .Configure<StorageOptions>(options => configuration.Bind(optionsKey, options))
+                .TryAddSingleton<MigrationBuilder>();
 
             return services;
         }
