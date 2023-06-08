faa69532a8373dcfae40d8fbf0cbe085c0a7f7c7
Erik van der Boom <evdboom@hotmail.com>
2023-06-08 14:08:19 +0200

    small change to test

--- OptionA.Blazor.Test/Program.cs
+++ OptionA.Blazor.Test/Program.cs
@@ -1,8 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using OptionA.Blazor.Components.Buttons.Struct;
-using OptionA.Blazor.Components.Carousel.Struct;
-using OptionA.Blazor.Components.Menu.Struct;
+using OptionA.Blazor.Components;
 using OptionA.Blazor.Test;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -11,10 +9,11 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services
-    .AddBootstrapButtons()
-    .AddBootstrapMenu()
-    .AddBootstrapCarousel(config =>
+    .AddOptionABootstrapComponents(configuration: options =>
     {
-        config.AutoPlayText = "Autoplay";
+        options.CarouselConfiguration = (carousel) =>
+        {
+            carousel.AutoPlayText = "Autoplay";
+        };
     });
 await builder.Build().RunAsync();
