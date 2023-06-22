b2246b439002a096d448b79135c782644755e728
Erik van der Boom <evdboom@hotmail.com>
2023-06-22 15:12:09 +0200

    menu fix for firefox

--- OptionA.Blazor.Components/Menu/OptAMenuGroup.razor.js
+++ OptionA.Blazor.Components/Menu/OptAMenuGroup.razor.js
@@ -1,3 +1,10 @@
-﻿export const getScrollHeight = (element) => {    
-    return element.scrollHeight;
+﻿export const getScrollHeight = (element) => {
+    // First make the element visible (firefox doesn't like it otherwise)
+
+    element.style.overflow = "scroll";
+    element.style.visibility = "visible";
+    const scrollHeight = element.scrollHeight;
+    element.style.overflow = "";
+    element.style.visibility = "";
+    return scrollHeight;
 }
