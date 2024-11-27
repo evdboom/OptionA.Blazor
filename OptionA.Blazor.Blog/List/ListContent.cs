579028ba4db79a2b17c550bc4c88fb68e9c21a9c
Erik van der Boom <evdboom@hotmail.com>
2024-11-27 23:09:54 +0100

    Add ListContent class, ListType enum, and OptAList component
    Added a new namespace `OptionA.Blazor.Blog` and within it, created a
    new class `ListContent` that inherits from `Content`. This class
    includes properties for `ListType`, a list of items, and an override
    for `IsInvalid` to check if the list is empty.
    
    Introduced a new enum `ListType` within the `OptionA.Blazor.Blog`
    namespace to define the type of list, either `OrderedList` or
    `UnorderedList`.
    
    Created a new Razor component `OptAList.razor` within the
    `OptionA.Blazor.Blog` namespace. This component dynamically renders
    either an ordered list (`<ol>`) or an unordered list (`<ul>`) based
    on the `ListType` property of the `Content`. Each list item includes
    an input field and a remove button.
    
    Added a code-behind file `OptAList.razor.cs` for the `OptAList`
    component. This partial class includes a `Content` parameter of type
    `ListContent` and an injected `IBlogDataProvider` to provide data
    and attributes for the component.

---
+++ OptionA.Blazor.Blog/List/ListContent.cs
@@ -0,0 +1,22 @@
+﻿namespace OptionA.Blazor.Blog
+{
+    /// <summary>
+    /// Content for list parts
+    /// </summary>
+    public class ListContent : Content
+    {
+        /// <inheritdoc/>
+        public override ContentType Type => ContentType.List;
+        /// <summary>
+        /// Type of list, ordered or unordered
+        /// </summary>
+        public ListType ListType { get; set; }
+        /// <summary>
+        /// Items to display in the list
+        /// </summary>
+        public List<string> Items { get; } = [];
+
+        /// <inheritdoc/>
+        public override bool IsInvalid => Items.Count == 0;
+    }
+}
---
+++ OptionA.Blazor.Blog/List/ListType.cs
@@ -0,0 +1,17 @@
+﻿namespace OptionA.Blazor.Blog
+{
+    /// <summary>
+    /// Type of list
+    /// </summary>
+    public enum ListType
+    {
+        /// <summary>
+        /// Ordered list
+        /// </summary>
+        OrderedList,
+        /// <summary>
+        /// Unordered list
+        /// </summary>
+        UnorderedList,
+    }
+}
